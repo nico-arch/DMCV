42b4af8d0802a2c2132185a9265498a88a0a4320
Nick Anderson Azemar <newnick1996@gmail.com>
2021-01-04 17:19:39 +0000

    update

--- dia_mvc_app/models.py
+++ dia_mvc_app/models.py
@@ -9,15 +9,10 @@ class Dossier(models.Model):
     #type = models.CharField(max_length=200, help_text='Edition du livre')
     utilsateur  = models.OneToOneField(User, on_delete = models.CASCADE,
     primary_key = True, help_text='')
-
-    addresse = models.CharField(max_length=200, null=True,help_text='Numero, Rue')
-    
+    addresse    = models.CharField(max_length=200, null=True,help_text='Numero, Rue')
     identifiantCin_Nif = models.CharField(max_length=200, null=True, help_text='')
-
     Diagnostic   = models.ForeignKey('Diagnostic', on_delete=models.SET_NULL, null=True)
-
-    RendezVous = models.ForeignKey('Consultation', on_delete=models.SET_NULL, null=True)
-
+    RendezVous   = models.ForeignKey('Consultation', on_delete=models.SET_NULL, null=True)
     Prescription = models.ForeignKey('Prescription', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
@@ -27,18 +22,28 @@ class Dossier(models.Model):
         return 'Dossier de {0}'.format(self.utilsateur.username)
         #return '{0} ({1})'.format(self.id,self.book.title))
 
+
+
 class Diagnostic(models.Model):
     """Table contenant les champs à remplir par le médecin lors du diagnostics"""
+    dossier =
+    
+    
     def __str__(self):
         """Cette fonction est obligatoirement requise par Django.
            Elle retourne une chaîne de caractère pour identifier l'instance de la classe d'objet."""
         #return self.nom
         #return 'Dossier de {0}'.format(self.utilsateur.username)
         #return '{0} ({1})'.format(self.id,self.book.title))
+
 
 
 class RendezVous(models.Model):
     """Table contenant des informations pour la configuration des rendez-vous"""
+    
+    dossier =
+    date_ = 
+    
     def __str__(self):
         """Cette fonction est obligatoirement requise par Django.
            Elle retourne une chaîne de caractère pour identifier l'instance de la classe d'objet."""
@@ -46,9 +51,15 @@ class RendezVous(models.Model):
         #return 'Dossier de {0}'.format(self.utilsateur.username)
         #return '{0} ({1})'.format(self.id,self.book.title))
 
+
+
 class Prescription(models.Model):
     """Table contenant les prescriptions et les notes essentielles pour les patients."""
 
+   dossier = 
+   ordonnance =
+   notesImportantes =
+    
     def __str__(self):
         """Cette fonction est obligatoirement requise par Django.
            Elle retourne une chaîne de caractère pour identifier l'instance de la classe d'objet."""
