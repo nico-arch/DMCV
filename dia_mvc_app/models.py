46481d8f4832578f8b534d20a4311bc5e0e317da
Nick Anderson Azemar <newnick1996@gmail.com>
2021-01-04 17:09:38 +0000

    update

--- dia_mvc_app/models.py
+++ dia_mvc_app/models.py
@@ -5,15 +5,19 @@ from datetime import date
 # Create your models here.
 
 class Dossier(models.Model):
-    """Cet objet représente les dossier des patients."""
+    """Table contenant des informations pour la création de dossiers virtuels pour les patients de la clinique."""
     #type = models.CharField(max_length=200, help_text='Edition du livre')
     utilsateur  = models.OneToOneField(User, on_delete = models.CASCADE,
     primary_key = True, help_text='')
+
     addresse = models.CharField(max_length=200, null=True,help_text='Numero, Rue')
-    CIN = models.CharField(max_length=200, null=True, help_text='Numero, Rue')
+    
+    identifiantCin_Nif = models.CharField(max_length=200, null=True, help_text='')
 
     Diagnostic   = models.ForeignKey('Diagnostic', on_delete=models.SET_NULL, null=True)
-    Consultation = models.ForeignKey('Consultation', on_delete=models.SET_NULL, null=True)
+
+    RendezVous = models.ForeignKey('Consultation', on_delete=models.SET_NULL, null=True)
+
     Prescription = models.ForeignKey('Prescription', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
@@ -24,7 +28,7 @@ class Dossier(models.Model):
         #return '{0} ({1})'.format(self.id,self.book.title))
 
 class Diagnostic(models.Model):
-    """Cet objet représente les diagnostics des patients."""
+    """Table contenant les champs à remplir par le médecin lors du diagnostics"""
     def __str__(self):
         """Cette fonction est obligatoirement requise par Django.
            Elle retourne une chaîne de caractère pour identifier l'instance de la classe d'objet."""
@@ -33,8 +37,8 @@ class Diagnostic(models.Model):
         #return '{0} ({1})'.format(self.id,self.book.title))
 
 
-class Consultation(models.Model):
-    """Cet objet représente les consultations des patients."""
+class RendezVous(models.Model):
+    """Table contenant des informations pour la configuration des rendez-vous"""
     def __str__(self):
         """Cette fonction est obligatoirement requise par Django.
            Elle retourne une chaîne de caractère pour identifier l'instance de la classe d'objet."""
@@ -43,7 +47,7 @@ class Consultation(models.Model):
         #return '{0} ({1})'.format(self.id,self.book.title))
 
 class Prescription(models.Model):
-    """Cet objet représente les prescriptions des patients."""
+    """Table contenant les prescriptions et les notes essentielles pour les patients."""
 
     def __str__(self):
         """Cette fonction est obligatoirement requise par Django.
