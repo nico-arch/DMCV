3441951974bdfe83534cdda3f1031d96e094e46a
Nick Anderson Azemar <76443393+nico-arch@users.noreply.github.com>
2021-01-27 14:48:40 -0500

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
+# Register your models here.
+@admin.register(Dossier, Diagnostic, RendezVous, Prescription)
+
+#class ViewAdmin(admin.ModelAdmin):
+#    pass
+
+class ViewAdmin(ImportExportModelAdmin):
+    pass
