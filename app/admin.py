5e46dd31e272709354b0ec332e281597f4595f92
Nick Anderson Azemar <newnick1996@gmail.com>
2021-01-27 20:48:22 +0000

    .

--- app/admin.py
+++ app/admin.py
@@ -5,7 +5,10 @@ from .models import *
 # Register your models here.
 @admin.register(Dossier, Diagnostic, RendezVous, Prescription)
 
+
 class ViewAdmin(admin.ModelAdmin):
     pass
 
+admin.site.site_header = 'DMCV Administration Panel'
+admin.site.site_title = 'DMCV'
 
