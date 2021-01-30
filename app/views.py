a5e8a7e396054cf7fe4c21e02818d5232b1df15a
Nick Anderson Azemar <76443393+nico-arch@users.noreply.github.com>
2021-01-29 19:21:07 -0500

    .

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from typing import List
-
+import datetime
+import dateutil
 
 # Create your views here.
 def index(request):
@@ -11,14 +12,23 @@ def index(request):
 def profile(request):
 
   # Cherche le dossier de l'utilisateur
-    dossierUtl = Dossier.objects.filter(utilsateur = request.user)
-    
-    #diagnostics = list(Diagnostic.objects.all().filter( dossier= dossierUtl) )
-    diagnostics = Diagnostic.objects.all()
+    dossierUtl = Dossier.objects.all().filter(utilsateur = request.user)
+    dossierUtlnext = Dossier.objects.get(utilsateur = request.user)
+
+
+    # Cherche la liste de diagnostics de l'utilisateur
+    diagnostics_list = Diagnostic.objects.all().filter( dossier__in= dossierUtl)
+
+    #dossierUtlnext = Dossier.objects.get(utilsateur = request.user)
+    sex  = dossierUtlnext.sex
+    age  = int( (date.today() - dossierUtlnext.dateDeNaissance) // timedelta(days=365.2425) )
+
 
     context = {
         'dossier': dossierUtl,
-        'diagnostics': diagnostics,
+        'age': age,
+        'sex': sex,
+        'diagnostics': diagnostics_list,
     }
 
     return render(request, 'profile/index.html', context = context)
